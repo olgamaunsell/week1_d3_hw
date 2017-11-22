959d9c980281adde7d4fe68370332f01e6c808cd
Olga Maunsell <olgamaunsell@yahoo.co.uk>
2017-11-22 20:53:59 +0000

    Final commit

--- homework.rb
+++ homework.rb
@@ -140,4 +140,49 @@ users ["Olga"] = {
     }
 
     puts users["Olga"]
-    puts users["Erik"]
+
+    # C. Given the following data structure:
+
+    united_kingdom = [
+      {
+        name: "Scotland",
+        population: 5295000,
+        capital: "Edinburgh"
+      }, {
+        name: "Wales",
+        population: 3063000,
+        capital: "Swansea"
+      }, {
+        name: "England",
+        population: 53010000,
+        capital: "London"
+      }
+    ]
+
+    # 1. Change the capital of Wales from `"Swansea"` to `"Cardiff"`.
+
+
+    p united_kingdom
+
+    # 2. Create a Hash for Northern Ireland and add it to the `united_kingdom` array (The capital is Belfast, and the population is 1,811,000).
+
+    northern_ireland = {
+        name: "Northern Ireland",
+        population: 1811000,
+        capital: "Belfast"
+    }
+    united_kingdom.push(northern_ireland)
+    p united_kingdom
+
+    # 3. Use a loop to print the names of all the countries in the UK.
+
+  for country in united_kingdom
+    p country[:name]
+  end
+
+    # 4. Use a loop to find the total population of the UK.
+    total_pop = 0
+    for country in united_kingdom
+      total_pop += country[:population]
+    end
+    p total_pop
